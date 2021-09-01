8c26e8c40e67f795acbfe80028b5707ae5bc7927
pallomaruysa <pallomaruysa@gmail.com>
2021-09-01 14:17:04 -0300

    ajustando conteudo das pastas css/ e script/

--- login.js
+++
@@ -1,6 +0,0 @@
-const login = document.querySelector('usuario_login');
-const senha = document.querySelector('senha_login');
-
-function entrar(){
-	document.getElementById("btn-entrar").innerHTML = "Entrar"
-}
