f4a1b7da1d798c515ac4771fe04c90ebecbc016b
Eunice2308 <eunicesuzart2308@gmail.com>
2021-08-31 23:34:30 -0300

    criando script do login

---
+++ login.js
@@ -0,0 +1,6 @@
+const login = document.querySelector('usuario_login');
+const senha = document.querySelector('senha_login');
+
+function entrar(){
+	document.getElementById("btn-entrar").innerHTML = "Entrar"
+}
