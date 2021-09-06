d6087d3ee1fdacd66c391d25171cbb5ca63f7cd4
Taila Silva <you@example.com>
2021-09-06 14:29:41 -0300

    Configuração do database

---
+++ script/database/database-connection.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqP8yePexKPyzcSYzH0ykeJoj2GdeWemg",
+    authDomain: "sysunity-9d94f.firebaseapp.com",
+    databaseURL: "https://sysunity-9d94f-default-rtdb.firebaseio.com",
+    projectId: "sysunity-9d94f",
+    storageBucket: "sysunity-9d94f.appspot.com",
+    messagingSenderId: "896238855938",
+    appId: "1:896238855938:web:dddadf73a96d579cc4441b",
+    measurementId: "G-R62J5CJE1J"
+  };
+  firebase.initializeApp(firebaseConfig);
