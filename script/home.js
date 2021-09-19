235bdb949ca69276b290768c0d9f1e1c598e0545
Taila Silva <you@example.com>
2021-09-18 22:53:35 -0300

    Reformulação da sidebar

---
+++ script/home.js
@@ -0,0 +1,10 @@
+
+ var div = document.getElementById('button');
+    var icon = document.getElementById('icon');
+    var open = false;
+
+ div.addEventListener('click', function(){
+       if (open){
+           icon.clas
+       }
+});
