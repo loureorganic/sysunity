7155de82cb6684256a6758845c31c1442b34e692
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-20 16:48:14 -0300

    teste2 de alteracao da validacao do input

--- script/index.js
+++ script/index.js
@@ -2,15 +2,13 @@ import Login from "./database/login.js";
 
 
 document.getElementById("btn_Entrar").addEventListener("click", function() {
-	// const dadosdosusuario =  new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+	const dadosdosusuario =  new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 
-	const dadosdosusuario;
+	const campoUsuario_dados = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 
-	const campoUsuario_dados = document.getElementById('usuario_login').value;
-
-	if(campoUsuario_dados != "")
+	if(campoUsuario_dados == "")
 	{
-		campoUsuario_dados.className += " #usuario_login"; 
+		campoUsuario_dados.className += " #usuario_login, #usario_senha"; 
 		campoUsuario_dados.focus();
 	} else {
 		return dadosdosusuario =  new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
