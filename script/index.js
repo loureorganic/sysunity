26c4bde62393f20caed161cb64d69cb155001738
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-20 16:38:34 -0300

    alterando as variaveis da validacao dos inputs

--- script/index.js
+++ script/index.js
@@ -2,13 +2,14 @@ import Login from "./database/login.js";
 
 
 document.getElementById("btn_Entrar").addEventListener("click", function() {
-	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
-	dadosdosusuario = new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+	const dadosdosusuario =  new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 
-	if(dadosdosusuario != "")
+	const campoUsuario_dados = document.getElementById('usuario_login').value;
+
+	if(campoUsuario_dados != "")
 	{
-		dadosdosusuario.className += " #usuario_login, #usuario_senha"; 
-		dadosdosusuario.focus();
+		campoUsuario_dados.className += " #usuario_login"; 
+		campoUsuario_dados.focus();
 	} else {
 		return dadosdosusuario;
 	}
