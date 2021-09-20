de09654ccfeb3d19cedb5e727acf871aa30143c8
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-20 16:36:40 -0300

    alteracao dos dados da validacao

--- script/index.js
+++ script/index.js
@@ -2,14 +2,13 @@ import Login from "./database/login.js";
 
 
 document.getElementById("btn_Entrar").addEventListener("click", function() {
-	const  dadosdosusuario = new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
-		
-	const inputdadosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+	dadosdosusuario = new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 
-	if(inputdadosusuario != "")
+	if(dadosdosusuario != "")
 	{
-		inputdadosusuario.className += " #usuario_login, #usuario_senha"; 
-		inputdadosusuario.focus();
+		dadosdosusuario.className += " #usuario_login, #usuario_senha"; 
+		dadosdosusuario.focus();
 	} else {
 		return dadosdosusuario;
 	}
