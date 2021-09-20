a5e558cb3dcecfaec7b69c63c490fabd4c418687
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-20 16:40:03 -0300

    teste de alteracao

--- script/index.js
+++ script/index.js
@@ -2,7 +2,9 @@ import Login from "./database/login.js";
 
 
 document.getElementById("btn_Entrar").addEventListener("click", function() {
-	const dadosdosusuario =  new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+	// const dadosdosusuario =  new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+
+	const dadosdosusuario;
 
 	const campoUsuario_dados = document.getElementById('usuario_login').value;
 
@@ -11,6 +13,6 @@ document.getElementById("btn_Entrar").addEventListener("click", function() {
 		campoUsuario_dados.className += " #usuario_login"; 
 		campoUsuario_dados.focus();
 	} else {
-		return dadosdosusuario;
+		return dadosdosusuario =  new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 	}
 });
