0debddddff2188d0f3454725f856a364bc872a5c
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-20 17:49:25 -0300

    teste8

--- script/index.js
+++ script/index.js
@@ -3,15 +3,25 @@ import Login from "./database/login.js";
 
 document.getElementById("btn_Entrar").addEventListener("click", function() {
 
-	const dadosUsuario = (document.getElementById('usuario_login').value);
+	const dadosUsuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 	const dadosdosusuario;
 
+	dadosUsuario.addEventListener("input", function (event){
+
+		if(dadosUsuario.validity,typeMismatch){
+	
+			dadosUsuario.setCustomValidity("O campo precisa ser preenchido");
+		} else {
+			return dadosdosusuario = new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+		}
+	});
+/*
 	if(dadosUsuario == "")
 	{
 		dadosUsuario.focus();
 		dadosUsuario.class.Name += ' input_erro';
 	} else {
 		return dadosdosusuario = new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
-	}
+	}*/
 
 });
