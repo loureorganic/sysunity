421cdb9032e38354cef62df53966abe44f9f560a
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-20 16:49:23 -0300

    teste 3 validacao input

--- script/index.js
+++ script/index.js
@@ -2,14 +2,12 @@ import Login from "./database/login.js";
 
 
 document.getElementById("btn_Entrar").addEventListener("click", function() {
-	const dadosdosusuario =  new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 
-	const campoUsuario_dados = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
-
-	if(campoUsuario_dados == "")
+	if(dadosdosusuario == "")
 	{
-		campoUsuario_dados.className += " #usuario_login, #usario_senha"; 
-		campoUsuario_dados.focus();
+		dadosdosusuario.className += " #usuario_login, #usario_senha"; 
+		dadosdosusuario.focus();
 	} else {
 		return dadosdosusuario =  new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 	}
