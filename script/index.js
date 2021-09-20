f54a9f229086b6ff05103076e7e94c4512b37caf
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-20 15:27:44 -0300

    script com funcao de validacao - em andamento

--- script/index.js
+++ script/index.js
@@ -2,5 +2,16 @@ import Login from "./database/login.js";
 
 
 document.getElementById("btn_Entrar").addEventListener("click", function() {
-	const dadosdosusuario = new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+
+	dadosdosusuario = validacao();
+	
+	function validacao(){
+		if(dadosdosusuario == "")
+		{
+			return "O campo precisa ser preechido";
+		} else {
+			return dadosdosusuario = new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+		}
+	}	
 });
