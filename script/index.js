611600226437a90bce578311b9d14e2e99676073
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-20 18:14:03 -0300

    teste10

--- script/index.js
+++ script/index.js
@@ -3,15 +3,13 @@ import Login from "./database/login.js";
 
 document.getElementById("btn_Entrar").addEventListener("click", function() {
 
-	const usuario = document.querySelector('usuario_login');
+	const dadosdosusuario;
 
-	if(usuario == ""){
-		usuario.class.Name += ' input_erro';
-		usuario.focus();
+	if(dadosdosusuario == ""){
+		dadosdosusuario.class.Name += ' #input_erro';
+		dadosdosusuario.focus();
 	} else {
 		dadosdosusuario = new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 	}
-
-	const dadosdosusuario;
 
 });
