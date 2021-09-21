1d485a687cb09501a2b1a25ce41a537a86f3c6c0
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-20 22:29:03 -0300

    validando input

--- script/index.js
+++ script/index.js
@@ -5,15 +5,10 @@ document.getElementById("btn_Entrar").addEventListener("click", function() {
 
 	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 
-	dadosdosusuario = validarSenha();
-
-	function validarSenha(){
 		if(dadosdosusuario == ""){
-			dadosdosusuario.className += ' input_erro';
+			dadosdosusuario.style.borderColor = "red",
 			dadosdosusuario.focus();
-			return false;
 		} else {
-			dadosdosusuario = new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+			new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 		}
-	}
 });
