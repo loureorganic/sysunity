97272895810d76ecbcc70737068cb290d7316675
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-21 00:16:20 -0300

    teste 19 input

--- script/index.js
+++ script/index.js
@@ -5,12 +5,22 @@ document.getElementById("btn_Entrar").addEventListener("click", function() {
 
 	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 
+	let msg = document.getElementById("msgErro").innerText;
+
 		if(document.getElementById('senha_login').value == "" || document.getElementById('senha_login').value == " "){
-			document.getElementById('senha_login').style.borderColor = "red",
+			document.getElementById('senha_login').style.borderColor = "red";
 			document.getElementById('senha_login').focus();
-			document.getElementById('senha_login').innerText = "O campo tá vazio";
+			document.getElementById('senha_login') = mostraMsg();
 		} else {
 			new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 		}
 
+		function mostraMsg(){
+			if(msg = "none")
+			{
+				document.getElementById("msgErro").style.display = "inline"
+			} else {
+				document.getElementById("msgErro").style.display = "none";
+			}
+		}
 });
