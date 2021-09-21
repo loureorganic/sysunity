d21bead84925c922b3c29616289d140fad2c6d8a
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-21 00:47:00 -0300

    teste 24 input - em andamento

--- script/index.js
+++ script/index.js
@@ -5,12 +5,9 @@ document.getElementById("btn_Entrar").addEventListener("click", function() {
 
 	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 
-	let msg = "O campo precisa ser preenchido";
-
 		if(document.getElementById('senha_login').value == "" || document.getElementById('senha_login').value == " "){
 			document.getElementById('senha_login').style.borderColor = "red";
 			document.getElementById('senha_login').focus();
-			document.getElementById('senha_login').innerText = msg;
 		} else {
 			new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 		}
