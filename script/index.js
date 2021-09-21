33dee3f6b6e21404cde39113f3ff89ca30815b99
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-21 12:28:09 -0300

    teste27 input

--- script/index.js
+++ script/index.js
@@ -4,11 +4,11 @@ import Login from "./database/login.js";
 document.getElementById("btn_Entrar").addEventListener("click", function() {
 
 	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
-
+	let msg = document.getElementById("msgErro");
 		if(document.getElementById('senha_login').value == "" || document.getElementById('senha_login').value == " "){
 			document.getElementById('senha_login').style.borderColor = "red";
 			document.getElementById('senha_login').focus();
-			document.getElementById("msgErro").style.display = "none";
+			msg.style.display = "block";
 		} else {
 			new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 		}
