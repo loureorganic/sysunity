d909d37baff970cb84093028c5aa5279ac9094f3
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-21 00:17:46 -0300

    teste 20 input

--- script/index.js
+++ script/index.js
@@ -5,22 +5,13 @@ document.getElementById("btn_Entrar").addEventListener("click", function() {
 
 	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 
-	let msg = document.getElementById("msgErro").innerText;
 
 		if(document.getElementById('senha_login').value == "" || document.getElementById('senha_login').value == " "){
 			document.getElementById('senha_login').style.borderColor = "red";
 			document.getElementById('senha_login').focus();
-			document.getElementById('senha_login') = mostraMsg();
+			document.getElementById('senha_login').style.display = "inline";
 		} else {
 			new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 		}
 
-		function mostraMsg(){
-			if(msg = "none")
-			{
-				document.getElementById("msgErro").style.display = "inline"
-			} else {
-				document.getElementById("msgErro").style.display = "none";
-			}
-		}
 });
