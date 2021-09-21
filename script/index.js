4670774f36c6c3d1cd3af7857f9f38c42b664ca9
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-20 22:44:00 -0300

    teste6 validacao input

--- script/index.js
+++ script/index.js
@@ -5,7 +5,7 @@ document.getElementById("btn_Entrar").addEventListener("click", function() {
 
 	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 
-		if(document.getElementById('senha_login').value == ""){
+		if(document.getElementById('senha_login').value == "" || document.getElementById('senha_login').value == " "){
 			document.getElementById('senha_login').innerText = 'O campo precisa ser preenchido';
 			document.getElementById('senha_login').style.borderColor = "red",
 			document.getElementById('senha_login').focus();
