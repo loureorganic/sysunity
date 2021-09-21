f01000badf94e73b554f899a0a80255873fd4904
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-20 23:03:18 -0300

    teste9 validacao input

--- script/index.js
+++ script/index.js
@@ -6,9 +6,9 @@ document.getElementById("btn_Entrar").addEventListener("click", function() {
 	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 
 		if(document.getElementById('senha_login').value == "" || document.getElementById('senha_login').value == " "){
-			document.getElementById('msgErro').innerText;
 			document.getElementById('senha_login').style.borderColor = "red",
 			document.getElementById('senha_login').focus();
+			document.getElementById('senha_login').innerText = ' .msgErro';
 		} else {
 			new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 		}
