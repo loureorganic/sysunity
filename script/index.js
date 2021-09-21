21b1dfcd40c8cf7ff73218ece0d2fb0640d1e46f
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-20 22:32:22 -0300

    validando input 2

--- script/index.js
+++ script/index.js
@@ -3,12 +3,16 @@ import Login from "./database/login.js";
 
 document.getElementById("btn_Entrar").addEventListener("click", function() {
 
-	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+	const usuario = document.getElementById('usuario_login').value;
+	const senha = document.getElementById('senha_login').value;
 
-		if(dadosdosusuario == ""){
-			dadosdosusuario.style.borderColor = "red",
-			dadosdosusuario.focus();
-		} else {
+		if(document.getElementById('usuario_login').value == ""){
+			document.getElementById('usuario_login').value.style.borderColor = "red",
+			document.getElementById('usuario_login').value.focus();
+		} else if (document.getElementById('senha_login').value){
+			document.getElementById('senha_login').value.style.borderColor = "red",
+			document.getElementById('senha_login').value.focus();
+		}else{	
 			new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 		}
 });
