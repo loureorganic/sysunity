fd366246966cec530a10483c8427a9b4099b4fb3
Eunice2308 <eunicesuzart2308@gmail.com>
2021-09-23 17:06:56 -0300

    script para sidebar - em andamento/ teste

--- script/home.js
+++ script/home.js
@@ -9,123 +9,21 @@
        }
 });
 
-$(document).ready(function() {
-    const btn_usuario = $('dropdown-menu');
-    const dropDown = $('usuario_configuracoes');    
-   
-    btn_usuario.on('click', function(event){
-        dropDown.stop(true,true).slideToggle();
+$(document).ready(function(){
+    
+    const classeUsuario = $('.collapse');
+    const ul = $('#ul_usuario');
+
+    classeUsuario.on('click', function(event){
+        ul.stop(true,true).slideToggle();
         event.stopPropagation();
     });
 
-    if(btn_usuario == usuario)
+    if(classeUsuario == ul_suuario)
     {
-       window.location.href = 'editar_usuario.html';
-    } else {
-        window.location.href = 'index.html';
-    }
-});
-$(document).ready(function() {
-    const btn = $('link-dark rounded');
-    const dropDown = $('ul_usuario');    
-   
-    btn.on('click', function(event){
-        dropDown.stop(true,true).slideToggle();
-        event.stopPropagation();
-    });
-
-    if(btn == ul_usuario)
-    {
-       window.location.href = 'cadastrar_usuario.html';
+        window.location.href = 'cadastrar_usuario.html';
     } else {
         window.location.href = 'gerenciar_usuario.html';
     }
-});
 
-$(document).ready(function() {
-    const btn2= $('link-dark rounded');
-    const dropDown = $('ul_vendas');    
-   
-    btn2.on('click', function(event){
-        dropDown.stop(true,true).slideToggle();
-        event.stopPropagation();
-    });
-
-    if(btn2 == ul_vendas)
-    {
-       window.location.href = 'cadastrar_vendas.html';
-    } else {
-        window.location.href = 'gerenciar_vendas.html';
-    }
-});
-
-$(document).ready(function() {
-    const btn3 = $('link-dark rounded');
-    const dropDown = $('ul_produtos');    
-   
-    btn3.on('click', function(event){
-        dropDown.stop(true,true).slideToggle();
-        event.stopPropagation();
-    });
-
-    if(btn3 == ul_produtos)
-    {
-       window.location.href = 'cadastrar_produto.html';
-    } else if( btn3 == ul_produtos) {
-        window.location.href = 'cadastrar_producao.html';
-    } else {
-        window.location.href = 'gerenciar_produto.html';
-    }
-
-});
-
-$(document).ready(function() {
-    const btn4 = $('link-dark rounded');
-    const dropDown = $('ul_relatorios');    
-   
-    btn4.on('click', function(event){
-        dropDown.stop(true,true).slideToggle();
-        event.stopPropagation();
-    });
-
-    if(btn4 == ul_relatorios)
-    {
-       window.location.href = 'relatorios_gerais.html';
-    } else if(btn4 == ul_relatorios){
-        window.location.href = 'relatorios_vendas.html';
-    } else {
-        window.location.href = 'relatorios_produtos.html';
-    }
-});
-
-$(document).ready(function() {
-    const btn5 = $('link-dark rounded');
-   
-    btn5.on('click', function(){
-        window.location.href = 'encomendas.html';
-    });
-});
-
-$(document).ready(function() {
-    const btn6 = $('link-dark rounded');
-   
-    btn6.on('click', function(){
-        window.location.href = 'historico.html';
-    });
-});
-
-$(document).ready(function() {
-    const btn7 = $('link-dark rounded');
-   
-    btn7.on('click', function(){
-        window.location.href = 'configuracao.html';
-    });
-});
-
-$(document).ready(function() {
-    const btn8 = $('link-dark rounded');
-   
-    btn8.on('click', function(){
-        window.location.href = 'suporte.html';
-    });
-});
+})
