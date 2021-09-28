cb41fc41884befaa28520f2379c17817a5b37cf8
Taila Silva <you@example.com>
2021-09-28 00:23:22 -0300

    Alterção do index.js da master

--- script/index.js
+++ script/index.js
@@ -1,6 +1,16 @@
-import Login from "./database/databaseLogin.js";
+import Login from "./database/login.js";
 
 
 document.getElementById("btn_Entrar").addEventListener("click", function() {
-	const dadosdosusuario = new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+
+	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+	let msg = document.getElementById("msgErro");
+		if(document.getElementById('senha_login').value == "" || document.getElementById('senha_login').value == " "){
+			document.getElementById('senha_login').style.borderColor = "red";
+			document.getElementById('senha_login').focus();
+			msg.style.display = "block";
+		} else {
+			new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
+		}
+
 });
