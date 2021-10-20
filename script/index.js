f7d467956c83925e608b156e03d6eab94cb9b3a3
Eunice2308 <eunicesuzart2308@gmail.com>
2021-10-19 23:18:11 -0300

    alterando variavel da msg de erro

--- script/index.js
+++ script/index.js
@@ -4,11 +4,11 @@ import Login from "./database/databaseLogin.js";
 document.getElementById("btn_Entrar").addEventListener("click", function() {
 
 	const dadosdosusuario = (document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
-	let msg = document.getElementById("msgErro");
+	let AlertaSenhaLogin = document.getElementById("alertalogin");
 		if(document.getElementById('senha_login').value == "" || document.getElementById('senha_login').value == " "){
 			document.getElementById('senha_login').style.borderColor = "red";
 			document.getElementById('senha_login').focus();
-			msg.style.display = "block";
+			alertalogin.style.display = "block";
 		} else {
 			new Login(document.getElementById('usuario_login').value, document.getElementById('senha_login').value);
 		}
