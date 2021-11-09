4c77ac86588e983422e517fc6f6b739236f76997
eugenio-cyber <wesleyeugenio3@gmail.com>
2021-11-09 02:38:10 -0300

    EM ANDAMENTO - arrumando o modal

--- script/database/databaseManagement-production.js
+++ script/database/databaseManagement-production.js
@@ -4,100 +4,130 @@ var btnsee = '<button type="button" id="btnSee" class="visibility" data-bs-toggl
 var btnedit = '<button type="button" id="btnEdit" class="edit" data-bs-toggle="modal" data-bs-target="#MyModalEditar"></button>';
 var btndelete = '<button id="btnDelete" class="delete" type="button" data-bs-toggle="modal" data-bs-target="#MyModalDeletar"></button>';
 
-var modalsee = `
-<div class="modal" id="MyModal">
-
+var modalsee = ` <div class="modal" id="MyModal">
 <div class="modal-dialog modal-dialog-centered">
-
   <div class="modal-content">
-
     <div class="modal-header">
-      <h5 class="modal-title">Visualizar Produto</h5>
+      <h5 class="modal-title">Visualizar Produção</h5>
       <button type="button" class="delete" data-bs-dismiss="modal"></button>
     </div>
-
     <div class="modal-body">
-
       <form>
-
         <div class="user-email">
-          <label for="">Nome
-            <input id="nome" type="text" placeholder="" disabled/>
+          <label for="">Item
+            <input id="item" type="text" placeholder="" disabled />
           </label>
-          <label for="">Tipo
-            <input id="tipo" type="text" placeholder="" disabled/>
+          <label for="">Lote
+            <input id="lote" type="text" placeholder="" disabled />
           </label>
-          <label for="">Valor
-            <input id="valor" type="text" placeholder="" disabled/>
+          <label for="">Funcionário
+            <input id="funcionario" type="text" placeholder="" disabled />
           </label>
         </div>
-
+        <div class="user-email">
+          <label for="">Quantidade em Estoque
+            <input id="qntEstoque" type="text" placeholder="" disabled />
+          </label>
+          <label for="">Quantidade Produzida
+            <input id="qntProduzida" type="text" placeholder="" disabled />
+          </label>                          
+        </div>
+        <div class="user-email">
+          <label for="">Data de Produção
+            <input id="dataProdução" type="text" placeholder="" disabled />
+          </label>
+          <label for="">Data de Venciemento
+            <input id="dataVenciemnto" type="text" placeholder="" disabled />
+          </label>                          
+        </div>
+        <div class="user-email">
+          <label for="">Prazo
+            <input id="prazo" type="text" placeholder="" disabled />
+          </label>                          
+          <label for="">Total da Produção
+            <input id="totalProducao" type="text" placeholder="" disabled />
+          </label>
+        </div>
       </form>
-
     </div>
-
   </div>
-
 </div>
+</div> `;
 
-</div>`;
-
-var modaledit = `
-<div class="modal" id="MyModalEditar">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title2">Editar Produto</h5>
-          <button type="button" class="delete" data-bs-dismiss="modal"></button>
-      </div>
-      <div class="modal-body">
-        <form>
-          <div class="user-email">
-            <label for="">Nome
-              <input id="nome" type="text" placeholder="">
-              <p id="msgErro" style="display: none;">O Campo precisa ser preenchido</p>
-            </label>
-            <label for="">Tipo
-              <input id="tipo" type="text" placeholder="">
-              <p id="msgErro2" style="display: none;">O Campo precisa ser preenchido</p>
-            </label>
-            <label for="">Valor
-              <input id="valor" type="text" placeholder="">
-              <p id="msgErro2" style="display: none;">O Campo precisa ser preenchido</p>
-            </label>
-          </div>
-          
-          <div class="save">
-            <div class="btn-group">
-              <button class="btn btn-primary" id="btn_salvar" type="button" >SALVAR</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>`;
-
-var modaldelete = `
-<div class="modal" id="MyModalDeletar">
+var modaledit = ` <div class="modal" id="MyModalEditar">
 <div class="modal-dialog modal-dialog-centered">
   <div class="modal-content">
     <div class="modal-header">
-      <h5 class="modal-title3">Deletar Produto</h5>
-        <button type="button" class="delete" data-bs-dismiss="modal"></button>
+      <h5 class="modal-title2">Editar Produção</h5>
+      <button type="button" class="delete" data-bs-dismiss="modal"></button>
     </div>
     <div class="modal-body">
       <form>
-        <h3>Tem certeza que deseja deletar esse produto?</h3>
-        <div class="alternative">
-          <button type="button" id="confirm" class="btn btn-primary">SIM</button>
-          <button type="button" id="deny" class="btn btn-secondary" data-bs-dismiss="modal">NÃO</button>
+        <div class="user-email">
+          <label for="">Item
+            <input id="item" type="text" placeholder="" />
+          </label>
+          <label for="">Lote
+            <input id="lote" type="text" placeholder="" />
+          </label>
+          <label for="">Funcionário
+            <input id="funcionario" type="text" placeholder="" />
+          </label>
+        </div>
+        <div class="user-email">
+          <label for="">Quantidade em Estoque
+            <input id="qntEstoque" type="text" placeholder="" />
+          </label>
+          <label for="">Quantidade Produzida
+            <input id="qntProduzida" type="text" placeholder="" />
+          </label>                          
+        </div>
+        <div class="user-email">
+          <label for="">Data de Produção
+            <input id="dataProdução" type="text" placeholder="" />
+          </label>
+          <label for="">Data de Venciemento
+            <input id="dataVenciemnto" type="text" placeholder="" />
+          </label>                          
+        </div>
+        <div class="user-email">
+          <label for="">Prazo
+            <input id="prazo" type="text" placeholder="" />
+          </label>                          
+          <label for="">Total da Produção
+            <input id="totalProducao" type="text" placeholder="" />
+          </label>
+        </div>
+        <div class="save">
+          <div class="btn-group">
+            <button class="btn btn-primary" id="btn_salvar" type="button">SALVAR</button>
+          </div>
         </div>
       </form>
     </div>
   </div>
 </div>
-</div>`;
+</div> `;
+
+var modaldelete = ` <div class="modal" id="MyModalDeletar">
+<div class="modal-dialog modal-dialog-centered">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title3">Deletar Produção</h5>
+      <button type="button" class="delete" data-bs-dismiss="modal"></button>
+    </div>
+    <div class="modal-body">
+      <form>
+        <h3>Tem certeza que deseja deletar a produção?</h3>
+        <div class="alternative">
+          <button type="button" class="btn btn-primary">SIM</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">NÃO</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+</div> `;
 
 export default class databasemanagementproduction {
 
@@ -115,7 +145,7 @@ export default class databasemanagementproduction {
           tr.classList.add('dot');
           dados =
           "<td>" + produto.name + "</td>" + 
-          "<td>" + produto.value + "</td>" + "<td>" + btnsee + btnedit + btndelete + "</td>";
+          "<td>" + produto.type + "</td>" + "<td>" + btnsee + btnedit + btndelete + "</td>";
 
           tr.innerHTML = dados;
           var_lista.appendChild(tr);
@@ -153,92 +183,92 @@ export default class databasemanagementproduction {
   }
 
   funcmodalsee(dados){
-    const firebaseref = firebase.database().ref("product");
-    firebaseref.once('value',(resultado)=>{
+    // const firebaseref = firebase.database().ref("product");
+    // firebaseref.once('value',(resultado)=>{
 
-      resultado.forEach(element => {
+    //   resultado.forEach(element => {
 
-        if(element.child("value").val() == dados) {
+    //     if(element.child("value").val() == dados) {
 
-          document.getElementById("nome").value = element.val().name;
-          document.getElementById("tipo").value = element.val().type;
-          document.getElementById("valor").value = element.val().value;
+    //       document.getElementById("nome").value = element.val().name;
+    //       document.getElementById("tipo").value = element.val().type;
+    //       document.getElementById("valor").value = element.val().value;
 
-        }
+    //     }
             
-      });
+    //   });
 
-    })
+    // })
 
   }
 
   funcmodaledit(a){
 
-    var self = this;
+    // var self = this;
 
-    const firebaseref = firebase.database().ref("product");
-    firebaseref.once('value',(resultado)=>{
+    // const firebaseref = firebase.database().ref("product");
+    // firebaseref.once('value',(resultado)=>{
       
-      resultado.forEach(element => {
+    //   resultado.forEach(element => {
 
-        if(element.child("value").val() == a) {
+    //     if(element.child("value").val() == a) {
 
-          document.getElementById("nome").placeholder = element.val().name;
-          document.getElementById("tipo").placeholder = element.val().type;
-          document.getElementById("valor").placeholder = element.val().value; 
+    //       document.getElementById("nome").placeholder = element.val().name;
+    //       document.getElementById("tipo").placeholder = element.val().type;
+    //       document.getElementById("valor").placeholder = element.val().value; 
           
-          var btn = document.getElementById("btn_salvar");
-          btn.addEventListener("click", function(e){
-            var inputnome = document.getElementById("nome").value;
-            var inputtipo = document.getElementById("tipo").value;
-            var inputvalor = document.getElementById("valor").value;
+    //       var btn = document.getElementById("btn_salvar");
+    //       btn.addEventListener("click", function(e){
+    //         var inputnome = document.getElementById("nome").value;
+    //         var inputtipo = document.getElementById("tipo").value;
+    //         var inputvalor = document.getElementById("valor").value;
 
-            if(inputnome === ""){
-              inputnome = element.val().name;
-            } 
-            if (inputtipo === ""){
-              inputtipo = element.val().type;
-            } 
-            if (inputvalor === ""){
-              inputvalor = element.val().value;
-            }
+    //         if(inputnome === ""){
+    //           inputnome = element.val().name;
+    //         } 
+    //         if (inputtipo === ""){
+    //           inputtipo = element.val().type;
+    //         } 
+    //         if (inputvalor === ""){
+    //           inputvalor = element.val().value;
+    //         }
 
-            self.funcUpd(element.key, inputnome, inputtipo, inputvalor);
-          })
+    //         self.funcUpd(element.key, inputnome, inputtipo, inputvalor);
+    //       })
             
-        }
+    //     }
           
-      });
+    //   });
       
-    })
+    // })
 
   }
 
   funcUpd(chave, nome, tipo, valor){
-    const database = firebase.database();
-    let active = false;
-    database.ref('product/'+chave+'/name').set(nome);
-    database.ref('product/'+chave+'/type').set(tipo);
-    database.ref('product/'+chave+'/value').set(valor);
+    // const database = firebase.database();
+    // let active = false;
+    // database.ref('product/'+chave+'/name').set(nome);
+    // database.ref('product/'+chave+'/type').set(tipo);
+    // database.ref('product/'+chave+'/value').set(valor);
     
-    window.location.reload(active);
+    // window.location.reload(active);
   };
 
   funcmodaldelete(dados){
 
-    let valu = '';
-    let active = false;
-    const database = firebase.database();
-    const firebaseref = firebase.database().ref("product");
-    firebaseref.once('value',(resultado)=>{
-    resultado.forEach(element => {
-      if(element.child("value").val() == dados) {
-        valu = element.key;
-        database.ref('product/'+valu).remove();
-        window.location.reload(active);
-      }
-    });
-  })
+  //   let valu = '';
+  //   let active = false;
+  //   const database = firebase.database();
+  //   const firebaseref = firebase.database().ref("product");
+  //   firebaseref.once('value',(resultado)=>{
+  //   resultado.forEach(element => {
+  //     if(element.child("value").val() == dados) {
+  //       valu = element.key;
+  //       database.ref('product/'+valu).remove();
+  //       window.location.reload(active);
+  //     }
+  //   });
+  // })
 
  }
 
