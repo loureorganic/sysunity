66cde7c1330dbbf00ce3dafd28643c14b428f47b
Taila Silva <you@example.com>
2021-11-15 10:44:20 -0300

    Backend da tela de produto

--- script/database/databaseCreate-product.js
+++ script/database/databaseCreate-product.js
@@ -3,11 +3,10 @@ export default class databasecreateproduct{
     createproduct(name, type, value){
         const database = firebase.database();
         let active = false;
-        database.ref('product/-MmzKkAAgksDrVcpWdtL').remove();
         let newProductKey = database.ref().child('product').push().key;
-        database.ref('product/'+newProductKey+'/name').set(name);
-        database.ref('product/'+newProductKey+'/type').set(type);
-        database.ref('product/'+newProductKey+'/value').set(value);
+        database.ref('product/'+name).set(name);
+        database.ref('product/'+name+'/'+type).set(type);
+        database.ref('product/'+name+'/'+type+'/value').set(value);
 
         window.location.reload(active);
     }
