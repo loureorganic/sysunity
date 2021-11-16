d075a92768cb9708e2b5f54151b700e8250758fc
Eunice2308 <eunicesuzart2308@gmail.com>
2021-11-16 01:01:15 -0300

    alterando os id do script da tela sale registration

--- script/sale_registration.js
+++ script/sale_registration.js
@@ -29,22 +29,23 @@ document.getElementById("public2").addEventListener("click", function() {
 document.getElementById("btn_cadastrar").addEventListener("click", function() {
 
 	const dadosVenda = (document.getElementById("seller").value, document.getElementById('date').value,
-	document.getElementById('total-general').value, document.getElementById('product').value,
-	document.getElementById('type').value, document.getElementById('quantity').value,
-	document.getElementById('price').value, document.getElementById('total').value,
-	document.getElementById('total-value').value, document.getElementById('payment_method').value,
-	document.getElementById('discount').value, document.getElementById('total-paid').value);
+	document.getElementById('selectProdutoVendas').value, document.getElementById('selectTipoVendas').value,
+	document.getElementById('quantity').value, document.getElementById('price').value, document.getElementById('total').value, 
+	document.getElementById('total-general-id').value, document.getElementById('total-general-id-2').value,
+	document.getElementById('selectPagamentoVendas').value, document.getElementById('total-value-id').value, 
+	document.getElementById('discount').value, document.getElementById('total-paid-input').value);
  
     
     let AlertaVendedor = document.getElementById("alerta_vendedor");
 	let AlertaData = document.getElementById("alerta_data");
 	let AlertaTotalGeral = document.getElementById("alerta_totalgeral");
+	let AlertaTotalGeral = document.getElementById("alerta_totalgeral_2");
 	let AlertaQuantidade = document.getElementById("alerta_quantidade");
 	let AlertaPrecoUnidade = document.getElementById("alerta_precounidade");
 	let AlertaTotal = document.getElementById("alerta_total");
 	let AlertaValorTotal = document.getElementById("alerta_valortotal");
 	let AlertaDesconto = document.getElementById("alerta_desconto");
-	let AlertaTotalPago = document.getElementById("alerta_totalpago");
+	let AlertaTotalPago = document.getElementById("total-paid-p");
 	
 
 	if(document.getElementById('seller').value == "" || document.getElementById('seller').value == " "){
@@ -57,11 +58,7 @@ document.getElementById("btn_cadastrar").addEventListener("click", function() {
 			document.getElementById('date').focus();
 			AlertaData.style.display = "block";
 	}
-	if(document.getElementById('total-general').value == "" || document.getElementById('total-general').value == " "){
-			document.getElementById('total-general').style.borderColor = "red";
-			document.getElementById('total-general').focus();
-			AlertaTotalGeral.style.display = "block";
-	}
+
 	if(document.getElementById('quantity').value == "" || document.getElementById('quantity').value == " "){
 			document.getElementById('quantity').style.borderColor = "red";
 			document.getElementById('quantity').focus();
@@ -72,25 +69,40 @@ document.getElementById("btn_cadastrar").addEventListener("click", function() {
 			document.getElementById('price').focus();
 			AlertaPrecoUnidade.style.display = "block";
 	}
+
 	if(document.getElementById('total').value == "" || document.getElementById('total').value == " "){
 			document.getElementById('total').style.borderColor = "red";
 			document.getElementById('total').focus();
 			AlertaTotal.style.display = "block";
 	}
 
-	if(document.getElementById('total-value').value == "" || document.getElementById('total-value').value == " "){
-			document.getElementById('total-value').style.borderColor = "red";
-			document.getElementById('total-value').focus();
+	if(document.getElementById('total-general-id').value == "" || document.getElementById('total-general-id').value == " "){
+		document.getElementById('total-general-id').style.borderColor = "red";
+		document.getElementById('total-general-id').focus();
+		AlertaTotalGeral.style.display = "block";
+	}
+
+	if(document.getElementById('discount').value == "" || document.getElementById('discount').value == " "){
+		document.getElementById('discount').style.borderColor = "red";
+		document.getElementById('discount').focus();
+		AlertaDesconto.style.display = "block";
+	}
+
+	if(document.getElementById('total-general-id-2').value == "" || document.getElementById('total-general-id-2').value == " "){
+		document.getElementById('total-general-id-2').style.borderColor = "red";
+		document.getElementById('total-general-id-2').focus();
+		AlertaTotalGeral.style.display = "block";
+	}
+
+	if(document.getElementById('total-value-id').value == "" || document.getElementById('total-value-id').value == " "){
+			document.getElementById('total-value-id').style.borderColor = "red";
+			document.getElementById('total-value-id').focus();
 			AlertaValorTotal.style.display = "block";
 	}
-	if(document.getElementById('discount').value == "" || document.getElementById('discount').value == " "){
-			document.getElementById('discount').style.borderColor = "red";
-			document.getElementById('discount').focus();
-			AlertaDesconto.style.display = "block";
-	}
-	if(document.getElementById('total-paid').value == "" || document.getElementById('total-paid').value == " "){
-			document.getElementById('total-paid').style.borderColor = "red";
-			document.getElementById('total-paid').focus();
+
+	if(document.getElementById('total-paid-input').value == "" || document.getElementById('total-paid-input').value == " "){
+			document.getElementById('total-paid-input').style.borderColor = "red";
+			document.getElementById('total-paid-input').focus();
 			AlertaTotalPago.style.display = "block";
 	}
 
