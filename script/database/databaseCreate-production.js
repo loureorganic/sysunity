7a5f9d0b522afce7ace9c4aabf4629e09f313add
Taila Silva <you@example.com>
2021-11-22 22:26:57 -0300

    produtos do banco na tela de produção

--- script/database/databaseCreate-production.js
+++ script/database/databaseCreate-production.js
@@ -2,18 +2,17 @@
 var label = document.getElementById("label1");
 var label1 = document.getElementById("label2");
 var item = document.getElementById('unique');
-var item1 = document.getElementById('unique1');
 export default class databasecreateproduction{
 
     secondData(data){
       const firebaseref = firebase.database().ref("product");
       firebaseref.once('value', function(all){
         var select1 = document.createElement('select');
-         Object.keys(all.val()[data]).find((a)=>{
-          select1.setAttribute('id', 'unique');
+        select1.setAttribute('id', 'unique');
+        Object.keys(all.val()[data]).find((a)=>{
           var dados = "<option " + "id="+ a +" >" + a + "</option>"
-          select1.innerHTML += dados;
-            label1.appendChild(select1);
+          select1.innerHTML = dados;
+          label1.appendChild(select1);
         })
     })}
 
@@ -29,7 +28,18 @@ export default class databasecreateproduction{
           label.appendChild(select);
         })
     })
+    
   }
 
-   signup(){}
+   signup(){ 
+
+    const database = firebase.database();
+    let newClientKey = database.ref().child('production').push().key;
+    database.ref('production/'+newClientKey+'/batch').set("Kibe");
+    database.ref('production/'+newClientKey+'/batch').set("Carne");
+      database.ref('production/'+newClientKey+'/time').set(90);
+    database.ref('production/'+newClientKey+'/lastname').set(1259);
+    database.ref('production/'+newClientKey+'/unity').set(3);
+    database.ref('production/'+newClientKey+'/packages').set(20);
+   }
 }
--- script/production_registration.js
+++ script/production_registration.js
@@ -5,7 +5,6 @@ const teste = new databasecreateproduction();
 const dados = new databaseuser();
 
 dados.objectuser();
-teste.data();
 document.getElementById("deadline").addEventListener("click", function() {
     document.getElementById('boxday').style.display = "flex";
 	document.getElementById('boxfabrication').style.display = "none";
@@ -18,18 +17,20 @@ document.getElementById("date").addEventListener("click", function() {
 })
 
 Number(document.getElementById('unity').value);
-console.log(document.getElementById('batch1').value)
 
 
-teste.secondData(document.getElementById('unique').value);
+
+teste.data();
+teste.signup();
+console.log(document.getElementById('unique').value);
+// document.getElementById('unique').addEventListener("change", function() {
+//     console.log('u');
+// });
+
 document.getElementById("btn_cadastrar").addEventListener("click", function() {
-    teste.secondData(document.getElementById('unique1').value);
-    console.log(document.getElementById('batch1').value)
-    console.log(document.getElementById('unique').value, document.getElementById("unique1").value, document.getElementById("batch1").value,
-		document.getElementById("deadline").value, document.getElementById("date").value, document.getElementById('days').value,
-        document.getElementById('fabricationdate').value, document.getElementById('deadlinedate').value, document.getElementById('unity').value, 
-        document.getElementById('package').value);
 
+    
+    
     let AlertaNomeProduto = document.getElementById("alertaproduto");
     let AlertaTipoProduto = document.getElementById("alertatipoproduto");
     let AlertaLote = document.getElementById("alertavalorproduto");
@@ -37,48 +38,42 @@ document.getElementById("btn_cadastrar").addEventListener("click", function() {
     let AlertaDataFabricação = document.getElementById("alerta_data_fabricacao");
     let AlertaDataVencimento = document.getElementById("alerta_data_vencimento");
     let AlertaUnidade = document.getElementById("alerta_unidade");
-
+    
 	if(document.getElementById('name').value == "" || document.getElementById('name').value == " "){
-			document.getElementById('name').style.borderColor = "red";
-			document.getElementById('name').focus();
-			AlertaNomeProduto.style.display = "block";
+        document.getElementById('name').style.borderColor = "red";
+        document.getElementById('name').focus();
+        AlertaNomeProduto.style.display = "block";
 	}
-
+    
 	if(document.getElementById('tipe').value == "" || document.getElementById('tipe').value == " "){
-			document.getElementById('tipe').style.borderColor = "red";
-			document.getElementById('tipe').focus();
-			AlertaTipoProduto.style.display = "block";
+        document.getElementById('tipe').style.borderColor = "red";
+        document.getElementById('tipe').focus();
+        AlertaTipoProduto.style.display = "block";
 	}
 
 	if(document.getElementById('batch1').value == "" || document.getElementById('batch1').value == " "){
 			document.getElementById('batch1').style.borderColor = "red";
 			document.getElementById('batch1').focus();
 			AlertaLote.style.display = "block";
-	}
-
-    if(document.getElementById('days').value == "" || document.getElementById('days').value == " "){
-        document.getElementById('days').style.borderColor = "red";
-        document.getElementById('days').focus();
-        AlertaDiasVálidos.style.display = "block";
-    }
+        }
 
     if(document.getElementById('fabrication').value == "" || document.getElementById('fabrication').value == " "){
             document.getElementById('fabrication').style.borderColor = "red";
             document.getElementById('fabrication').focus();
             AlertaDataFabricação.style.display = "block";
     }
-
+    
         if(document.getElementById('pay').value == "" || document.getElementById('pay').value == " "){
             document.getElementById('pay').style.borderColor = "red";
             document.getElementById('pay').focus();
             AlertaDataVencimento.style.display = "block";
     }
-
+    
     if(document.getElementById('unity').value == "" || document.getElementById('unity').value == " "){
         document.getElementById('unity').style.borderColor = "red";
         document.getElementById('unity').focus();
         AlertaUnidade.style.display = "block";
     }
     
-
+    
 });
