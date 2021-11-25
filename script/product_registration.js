cd25cdeeeebb93311935a9d43ffe67a730d82010
eugenio-cyber <wesleyeugenio3@gmail.com>
2021-11-24 22:39:12 -0300

    Criação da estrutura p/ validar a opção e os inputs escolhidos

--- script/product_registration.js
+++ script/product_registration.js
@@ -28,32 +28,32 @@ document.getElementById("tipo").addEventListener("click", function() {
 
 document.getElementById("btn_cadastrar").addEventListener("click", function() {
 	
-	data.creatoption(data1, document.getElementById('tipe1').value, document.getElementById('value1').value );
-	// data.createproduct(document.getElementById('name').value, document.getElementById('tipe').value, document.getElementById('value').value)
+	// // data.creatoption(data1, document.getElementById('tipe1').value, document.getElementById('value1').value );
+	// // data.createproduct(document.getElementById('name').value, document.getElementById('tipe').value, document.getElementById('value').value)
 
-    let AlertaProduto = document.getElementById("alertaproduto");
-    let AlertaTipoProduto= document.getElementById("alertatipoproduto");
-    let AlertaValorProduto = document.getElementById("alertavalorproduto");
+    // let AlertaProduto = document.getElementById("alertaproduto");
+    // let AlertaTipoProduto= document.getElementById("alertatipoproduto");
+    // let AlertaValorProduto = document.getElementById("alertavalorproduto");
 	
-	if(document.getElementById('name').value == "" || document.getElementById('name').value == " "){
-		document.getElementById('name').style.borderColor = "red";
-		document.getElementById('name').focus();
-		AlertaProduto.style.display = "block";
-	}
+	// if(document.getElementById('name').value == "" || document.getElementById('name').value == " "){
+	// 	document.getElementById('name').style.borderColor = "red";
+	// 	document.getElementById('name').focus();
+	// 	AlertaProduto.style.display = "block";
+	// }
 	
-	else if(document.getElementById('tipe').value == "" || document.getElementById('tipe').value == " "){
-		document.getElementById('tipe').style.borderColor = "red";
-		document.getElementById('tipe').focus();
-		AlertaTipoProduto.style.display = "block";
-	}
+	// else if(document.getElementById('tipe').value == "" || document.getElementById('tipe').value == " "){
+	// 	document.getElementById('tipe').style.borderColor = "red";
+	// 	document.getElementById('tipe').focus();
+	// 	AlertaTipoProduto.style.display = "block";
+	// }
 	
-	else if(document.getElementById('value').value == "" || document.getElementById('value').value == " "){
-		document.getElementById('value').style.borderColor = "red";
-		document.getElementById('value').focus();
-		AlertaValorProduto.style.display = "block";
-	}
-	else {
-		// data.createproduct(document.getElementById('name').value, document.getElementById('tipe').value, document.getElementById('value').value);
-	}
+	// else if(document.getElementById('value').value == "" || document.getElementById('value').value == " "){
+	// 	document.getElementById('value').style.borderColor = "red";
+	// 	document.getElementById('value').focus();
+	// 	AlertaValorProduto.style.display = "block";
+	// }
+	// else {
+	// 	// data.createproduct(document.getElementById('name').value, document.getElementById('tipe').value, document.getElementById('value').value);
+	// }
 	
 });
