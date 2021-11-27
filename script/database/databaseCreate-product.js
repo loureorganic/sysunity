33772a8847b2c2bf12935e56e2af341d8d5200ce
eugenio-cyber <wesleyeugenio3@gmail.com>
2021-11-27 08:10:27 -0300

    Adicionado a validação dos inputs na tela de cadastro do produto

--- script/database/databaseCreate-product.js
+++ script/database/databaseCreate-product.js
@@ -3,36 +3,39 @@ var label = document.getElementById('label1');
 var select = document.getElementById("selectNameProduto")
 export default class databasecreateproduct{
 
-    createproduct(name, type, value){
-        const database = firebase.database();
-        let active = false;
-        let newProductKey = database.ref().child('product').push().key;
-        database.ref('product/'+name).set(name);
-        database.ref('product/'+name+'/'+type).set(type);
-        database.ref('product/'+name+'/'+type+'/value').set(value);
+  // CRIAÇÃO DO PRODUTO NO BANCO
+  createproduct(name, type, value){
+      const database = firebase.database();
+      let active = false;
+      let newProductKey = database.ref().child('product').push().key;
+      database.ref('product/'+name).set(name);
+      database.ref('product/'+name+'/'+type).set(type);
+      database.ref('product/'+name+'/'+type+'/value').set(value);
 
-        window.location.reload(active);
-    }
+      window.location.reload(active);
+  }
 
-    creatoption(optionselected, type, value){
-        const database = firebase.database();
-        let active = false;
-        database.ref('product/'+optionselected+'/'+type).set(type);
-        database.ref('product/'+optionselected+'/'+type+'/value').set(value);
-        window.location.reload(active);
-    }
+  // CRIAÇÃO DOS DADOS NO SELECT
+  creatoption(optionselected, type, value){
+      const database = firebase.database();
+      let active = false;
+      database.ref('product/'+optionselected+'/'+type).set(type);
+      database.ref('product/'+optionselected+'/'+type+'/value').set(value);
+      window.location.reload(active);
+  }
 
-    data(){
-        const firebaseref = firebase.database().ref("product");
-        firebaseref.once('value', function(all){
-          var select = document.createElement('select');
-          select.setAttribute('id', 'unique');
-          Object.keys(all.val()).map((keyname)=> {
-            var dados2 = "<option " + "id="+keyname +" >" + keyname + "</option>"
-            select.innerHTML += dados2;
-            label.appendChild(select);
-          })
-      })
-    }
+  // CRIAÇÃO DO SELECT
+  data(){
+      const firebaseref = firebase.database().ref("product");
+      firebaseref.once('value', function(all){
+        var select = document.createElement('select');
+        select.setAttribute('id', 'unique');
+        Object.keys(all.val()).map((keyname)=> {
+          var dados2 = "<option " + "id="+keyname +" >" + keyname + "</option>"
+          select.innerHTML += dados2;
+          label.appendChild(select);
+        })
+    })
+  }
     
 }
--- script/product_registration.js
+++ script/product_registration.js
@@ -6,7 +6,6 @@ dados.objectuser();
 const data = new databasecreateproduct();
 data.data();
 var data1;
-var productActive ;
 
 
 document.getElementById('boxproduct').style.display = 'none';
@@ -15,23 +14,59 @@ document.getElementById('boxproduct').style.display = 'none';
 document.getElementById('label1').addEventListener("click", function() {
   data1 = document.getElementById('unique').value;
 })
+
 document.getElementById("produtoTipo").addEventListener("click", function() {
 	document.getElementById('boxproduct').style.display = 'flex';
 	document.getElementById('boxdate').style.display = "flex";
 	document.getElementById('boxdate2').style.display = "none";
 })
+
 document.getElementById("tipo").addEventListener("click", function() {
 	document.getElementById('boxproduct').style.display = 'flex';
 	document.getElementById('boxdate').style.display = "none";
 	document.getElementById('boxdate2').style.display = "flex";
 })
-document.getElementById("produto-tipo").addEventListener("click", function(){
-	productActive = document.getElementById("produtoTipo").checked;
-})
+
+
 document.getElementById("btn_cadastrar").addEventListener("click", function() {
- if(productActive){
-	 data.createproduct(document.getElementById('name').value, document.getElementById('tipe').value, document.getElementById('value').value)
- } else {
-	 data.creatoption(data1, document.getElementById('tipe1').value, document.getElementById('value1').value );
- }
+
+	// VALIDAÇÃO DA OPÇÃO PRODUTO-TIPO
+	if(document.getElementById("produtoTipo").checked){
+
+		if(document.getElementById('name').value == ''){
+	
+		document.getElementById('name').style.borderColor = "red";
+		document.getElementById('alertaproduto').style.display = 'flex';
+	
+		} if(document.getElementById('tipe').value == ''){
+	
+		document.getElementById('tipe').style.borderColor = "red";
+		document.getElementById('alertatipoproduto').style.display = 'flex';
+	
+		} if(document.getElementById('value').value == ''){
+	
+		document.getElementById('value').style.borderColor = "red";
+		document.getElementById('alertavalorproduto').style.display = 'flex';
+		} else {
+			data.createproduct(document.getElementById('name').value, document.getElementById('tipe').value, document.getElementById('value').value);
+		}
+	}
+
+	// VALIDAÇÃO DA OPÇÃO TIPO
+	if(document.getElementById("tipo").checked){
+	
+		if(document.getElementById('tipe1').value == ''){
+		document.getElementById('tipe1').style.borderColor = "red";
+		document.getElementById('alertatipoproduto').style.display = 'flex';
+	
+		}
+		
+		if(document.getElementById('value1').value == ''){
+	
+		document.getElementById('value1').style.borderColor = "red";
+		document.getElementById('alertavalorproduto1').style.display = 'flex';
+		} else {
+			data.creatoption(data1, document.getElementById('tipe1').value, document.getElementById('value1').value );
+		}
+	}
 });
