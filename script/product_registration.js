1b4c272f660013d37b0eb7f9756f72d44e92e47f
Taila Silva <you@example.com>
2021-11-26 23:42:28 -0300

    ajustes de layout da sprint_5

--- script/product_registration.js
+++ script/product_registration.js
@@ -6,39 +6,32 @@ dados.objectuser();
 const data = new databasecreateproduct();
 data.data();
 var data1;
-var productActive;
+var productActive ;
+
+
+document.getElementById('boxproduct').style.display = 'none';
+
+
 document.getElementById('label1').addEventListener("click", function() {
   data1 = document.getElementById('unique').value;
 })
-
-
 document.getElementById("produtoTipo").addEventListener("click", function() {
+	document.getElementById('boxproduct').style.display = 'flex';
 	document.getElementById('boxdate').style.display = "flex";
 	document.getElementById('boxdate2').style.display = "none";
 })
-
-
 document.getElementById("tipo").addEventListener("click", function() {
+	document.getElementById('boxproduct').style.display = 'flex';
 	document.getElementById('boxdate').style.display = "none";
 	document.getElementById('boxdate2').style.display = "flex";
 })
-
 document.getElementById("produto-tipo").addEventListener("click", function(){
 	productActive = document.getElementById("produtoTipo").checked;
-	console.log(productActive);
 })
-
 document.getElementById("btn_cadastrar").addEventListener("click", function() {
-	console.log(productActive);
-	
-
  if(productActive){
 	 data.createproduct(document.getElementById('name').value, document.getElementById('tipe').value, document.getElementById('value').value)
  } else {
 	 data.creatoption(data1, document.getElementById('tipe1').value, document.getElementById('value1').value );
  }
-  
-
-	
-	
 });
