b2e098530f6a14f8c552b51e8bc29f38cb583011
Eunice2308 <eunicesuzart2308@gmail.com>
2021-11-30 16:19:49 -0300

    script tela de relatorio de produto em andamento

---
+++ script/reports_product.js
@@ -0,0 +1,29 @@
+const ctx = document.getElementById('grafico').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['produto1', 'produto2', 'produto3'],
+        datasets: [{
+            label: 'Produtos',
+            data: [10, 20, 30],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
