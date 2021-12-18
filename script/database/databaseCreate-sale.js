c54e7798a6a2a219fe3fa03eb02d8c22250f935a
Taila Silva <you@example.com>
2021-12-18 16:55:07 -0300

    Repetition error

--- script/database/databaseCreate-sale.js
+++ script/database/databaseCreate-sale.js
@@ -118,14 +118,8 @@ export default class databasecreatesale{
             })
 
             arrayTypesValid.forEach((item)=>{
-                if(document.getElementById(item)){
-                    console.log(document.getElementById(item).value == item);
-                }
-                else{
                     var dados = "<option " + "id="+item +" >" + item + "</option>";
                     selecttype.innerHTML += dados;
-                }
-
             })
             
              
