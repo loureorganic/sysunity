a4e64d51ee176b25a89d21b3d35b279dff46095a
Taila Silva <you@example.com>
2021-12-30 00:14:57 -0300

    Adjust

---
+++ script/database/databaseHome.js
@@ -0,0 +1,29 @@
+
+export default class databasehome{
+
+  homePackage(){
+    var self = this;
+
+    const firebaseref = firebase.database().ref("sale");
+    firebaseref.once('value', function(all){
+      var venda = [];
+      var geral = [];
+      all.forEach(
+        function(curecord){
+          venda.push(curecord.val());
+      
+        }
+      )
+      venda.slice(0, 3).map((item, index) => {
+        let valuePosition = 'td' + index;
+        let today2 = new Date();
+        let date = today2.getFullYear()+'-'+(today2.getMonth()+1)+'-'+today2.getDate();
+        console.log(Number(item.dateDelivery));
+        console.log(Number(date))
+        console.log(Number(item.dateDelivery) - Number(date));
+        document.getElementById(valuePosition).innerText = 'A encomenda de n° '+ item.numberSale +' faltam ' + (index * 3 + 1) + ' dias até o prazo limite para a entrega.'
+        console.log({item});
+      })
+    })
+  }
+}
